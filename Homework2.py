aca808b06084f926705972860f17e06af10ca564
EgorBlv <egor.zuev1@yandex.ru>
2024-07-17 02:32:21 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+homework = "12,"
+hours = "1.5,"
+courses = "Python,"
+time_of_one_lesson = (1.5 / 12)
+print('Курс:',courses, 'всего задач:',homework,'затрачено часов:',hours, 'среднее время выполнения:',time_of_one_lesson, 'часа')
